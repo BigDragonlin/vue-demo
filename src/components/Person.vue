8a374de1633c808f098e1b8fee9291b677a0bd56
BigDragonlin <51269337+BigDragonlin@users.noreply.github.com>
2025-09-21 00:56:02 +0800

    监视复杂对象中的某个属性

--- src/components/Person.vue
+++ src/components/Person.vue
@@ -1,10 +1,13 @@
 <template>
     <div class="person">
-        <h1>监视【reactive】定义的对象</h1>
-        <h2>Name: {{ person.name }}</h2>
-        <h2>Age: {{ person.age }}</h2>
-        <button @click="changeName">点我name改李四名字</button>
-        <button @click="changePerson">点我person改李四所有信息</button>
+        <h1>姓名：{{ person.name }}</h1>
+        <h1>年龄：{{ person.age }}</h1>
+        <h1>汽车：{{ person.car.c1}}、 {{ person.car.c2 }}</h1>
+        <button @click="person.name += '~'">点我name改李四名字</button>
+        <button @click="person.age++">点我age加1</button>
+        <button @click="person.car.c1 += '~'">点我c1改宝马</button>
+        <button @click="person.car.c2 += '~'">点我c2改奔驰</button>
+        <button @click="changeCar">点我changeCar</button>
     </div>
 </template>
 
@@ -13,24 +16,36 @@
     import { reactive, watch } from 'vue';
     let person = reactive({
         name: '张三',
-        age: 18
+        age: 18,
+        car: {
+            c1: '宝马',
+            c2: '奔驰'
+        }
     })
-    function changeName() {
-        person.name = '李四'
-    }
-    function changePerson() {
-        //使用reactive时修改对象
-        Object.assign(person, {
-            name: '李四',
-            age: 20
-        })
+    function changeCar() {
+        person.car = {
+            c1: '宝马',
+            c2: '奔驰'
+        }
     }
 
-    // 监视 情况三：监视reactive定义的对象中某个属性改变。reactive默认开启隐式的深度监视，所以任何属性变化都会触发监视
-    watch(person, (newVal, oldVal) => {
+    // 监视 情况三：监视reactive定义的对象中基础数据类型的改变
+    watch(() => person.name, (newVal, oldVal) => {
+        console.log('person的name发生了变化', newVal, oldVal)
+    })
+    watch(() => person.age, (newVal, oldVal) => {
+        console.log('person的age发生了变化', newVal, oldVal)
+    })
+
+    // 监视 情况四：监视reactive定义的对象中对象类型的改变。匿名函数返回的是地址,所以地址变了就会触发监视
+    watch(() => person.car, (newVal, oldVal) => {
+        console.log('person的car发生了变化', newVal, oldVal)
+    })
+
+    //完美写法
+    watch(() => person, (newVal, oldVal) => {
         console.log('person的值发生了变化', newVal, oldVal)
-    })
-
+    },{ deep: true })
 </script>
 
 <style>
