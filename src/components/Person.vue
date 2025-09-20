d8e4bb9356b37971a70ecbcc1f9cc61b2c78bc63
BigDragonlin <51269337+BigDragonlin@users.noreply.github.com>
2025-09-20 16:06:25 +0800

    添加组合式写法和相应式写法，并学习注意要点

--- src/components/Person.vue
+++ src/components/Person.vue
@@ -10,23 +10,43 @@
         <!-- 点击查看电话号码 -->
          <button @click="showPhone">查看电话号码</button>
     </div>
+
+    <hr></hr>
+    <p>A: {{ a }}</p>
+    <p>B: {{ b }}</p>
+    <p>setUPName: {{ c }}</p>
+    <p>anotherName: {{ d }}</p>
+    <button @click="alertB">查看B</button>
 </template>
 
 <script>
 export default {
     name: 'Person',
+    data() {
+        return {
+            a: '显示A',
+            b: '显示B',
+            // 在选项式布局中，可以使用this.name获取setup中的name
+            c: this.name,
+            // 在选项式布局中，可以使用this.anotherName获取setup中的anotherName,但是一定要return
+            d: this.anotherName
+        }
+    },
+
+    methods: {
+        alertB() {
+            alert(this.a)
+        },
+    },
+
     setup() {
         let name = 'John'
         let age = 20
         let tel = '1234567890'
-        // 这个this是undefined 
-        // console.log(this)
-        // console.log(this)
+        let anotherName = 'anotherName'
+        // 在setup中，不可以通过this.name获取选项式布局中的name，this是undefined
         function changeName() {
-            console.log('changeName')
             name = 'Tom'
-            // 点击不会更改name，是因为name是响应式数据
-            console.log(name)
         }
         function changeAge() {
             age += 1
@@ -35,21 +55,13 @@ export default {
         function showPhone() {
             alert(tel)
         }
-        // return{
-        //     name,
-        //     age,
-        //     tel,
-        //     changeName,
-        //     changeAge,
-        //     showPhone
-        // }
-
-        // setup也可以直接返回渲染函数
-        // return function(){
-        //     "哈哈"
-        // }
-        return ()=>{
-            "哈哈"
+        return{
+            name,
+            age,
+            tel,
+            changeName,
+            changeAge,
+            showPhone
         }
     }
 }
