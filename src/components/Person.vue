5fb9f82cf2df6739092c3b97e5b3bec981318445
BigDragonlin <51269337+BigDragonlin@users.noreply.github.com>
2025-09-21 01:15:26 +0800

    响应式的追踪依赖

--- src/components/Person.vue
+++ src/components/Person.vue
@@ -1,37 +1,37 @@
 <template>
     <div class="person">
-        <h1>姓名：{{ person.name }}</h1>
-        <h1>年龄：{{ person.age }}</h1>
-        <h1>汽车：{{ person.car.c1}}、 {{ person.car.c2 }}</h1>
-        <button @click="person.name += '~'">点我name改李四名字</button>
-        <button @click="person.age++">点我age加1</button>
-        <button @click="person.car.c1 += '~'">点我c1改宝马</button>
-        <button @click="person.car.c2 += '~'">点我c2改奔驰</button>
-        <button @click="changeCar">点我changeCar</button>
+        <h1>当水温达到60度或者水位达到80cm时，给服务期发请求</h1>
+        <h1>当前的水温是：{{ temperature }}</h1>
+        <h1>当前的水位是：{{ water }}</h1>
+        <button @click="addTemperature">水温+10</button>
+        <button @click="addWater">水位+10</button>
     </div>
 </template>
 
 
 <script lang="ts" setup name = "Person">
-    import { reactive, watch } from 'vue';
-    let person = reactive({
-        name: '张三',
-        age: 18,
-        car: {
-            c1: '宝马',
-            c2: '奔驰'
-        }
-    })
-    function changeCar() {
-        person.car = {
-            c1: '宝马',
-            c2: '奔驰'
-        }
+    import { ref, watch, watchEffect } from 'vue';
+    let temperature = ref(10)
+    let water = ref(15)
+    function addTemperature() {
+        temperature.value += 10
+    }
+    function addWater() {
+        water.value += 10
     }
 
-    // 监视 情况五：监视reactive定义的对象中某些属性的改变
-    watch([()=>person.name, ()=>person.car.c1], (newVal, oldVal) => {
-        console.log('person的name和car.c1发生了变化', newVal, oldVal)
+    // watch的写法
+    // watch([()=>temperature.value, ()=>water.value], (newVal, oldVal) => {
+    //     if(newVal[0] >= 60 || newVal[1] >= 80) {
+    //         console.log('水温达到60度或者水位达到80cm时，给服务期发请求')
+    //     }
+    // })
+
+    // watchEffect的写法
+    watchEffect(() => {
+        if(temperature.value >= 60 || water.value >= 80) {
+            console.log('水温达到60度或者水位达到80cm时，给服务期发请求')
+        }
     })
 </script>
 
