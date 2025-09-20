503e561049f0f60dab46d19b75707e4fedb15ad6
BigDragonlin <51269337+BigDragonlin@users.noreply.github.com>
2025-09-20 17:37:16 +0800

    ref可以以创建对象类型的响应式数据，底层还是reactive

--- src/components/Person.vue
+++ src/components/Person.vue
@@ -5,54 +5,20 @@
         <p>Color: {{ car.color }}</p>
         <button @click="changePrice">涨价</button>
     </div>
-    <hr> </hr>
-    <ul>
-        <li v-for="game in games" :key="game.id">
-            Name: {{ game.name }}
-            Price: {{ game.price }}
-        </li>
-        <button @click="changeGamePrice">涨价</button>
-    </ul>
-
-    <hr> </hr>
-    <p>C: {{ test.a.b.c }}</p>
-    <button @click="changeC">涨价</button>
+    <hr>
 </template>
 
 
 <script lang="ts" setup name = "Person">
-    import { reactive } from 'vue';
+    import { ref } from 'vue';
 
-    let car = reactive({
+    let car = ref({
         name: '宝马',
         price: 1000000,
         color: '红色'
     })
-
-    let games = reactive([
-        {id: 1, name: '王者荣耀', price: 100},
-        {id: 2, name: '和平精英', price: 100},
-        {id: 3, name: '原神', price: 100},
-    ])
-
-    // reactive包裹对象叫做响应式对象，影响是深层次的
-    let test = reactive({
-        a :{
-            b :{
-                c : 100
-            }
-        }
-    })
     function changePrice() {
-        car.price += 10
-    }
-
-    function changeGamePrice() {
-        games[0].price += 10
-    }
-
-    function changeC() {
-        test.a.b.c += 10
+        car.value.price += 10
     }
 </script>
 
