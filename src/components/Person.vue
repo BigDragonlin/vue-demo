64083134ae9519f9a97c784eb35cf381638d2452
BigDragonlin <51269337+BigDragonlin@users.noreply.github.com>
2025-09-20 16:24:30 +0800

    在项目中添加了vite-plugin-vue-setup-extend插件，使用setup语法糖，简化了代码结构和注释。

--- src/components/Person.vue
+++ src/components/Person.vue
@@ -10,61 +10,24 @@
         <!-- 点击查看电话号码 -->
          <button @click="showPhone">查看电话号码</button>
     </div>
-
-    <hr></hr>
-    <p>A: {{ a }}</p>
-    <p>B: {{ b }}</p>
-    <p>setUPName: {{ c }}</p>
-    <p>anotherName: {{ d }}</p>
-    <button @click="alertB">查看B</button>
 </template>
 
-<script>
-export default {
-    name: 'Person',
-    data() {
-        return {
-            a: '显示A',
-            b: '显示B',
-            // 在选项式布局中，可以使用this.name获取setup中的name
-            c: this.name,
-            // 在选项式布局中，可以使用this.anotherName获取setup中的anotherName,但是一定要return
-            d: this.anotherName
-        }
-    },
 
-    methods: {
-        alertB() {
-            alert(this.a)
-        },
-    },
-
-    setup() {
-        let name = 'John'
-        let age = 20
-        let tel = '1234567890'
-        let anotherName = 'anotherName'
-        // 在setup中，不可以通过this.name获取选项式布局中的name，this是undefined
-        function changeName() {
-            name = 'Tom'
-        }
-        function changeAge() {
-            age += 1
-        }
-
-        function showPhone() {
-            alert(tel)
-        }
-        return{
-            name,
-            age,
-            tel,
-            changeName,
-            changeAge,
-            showPhone
-        }
+<script lang="ts" setup name = "Person123">
+    let name = 'John'
+    let age = 20
+    let tel = '1234567890'
+    // 在setup中，不可以通过this.name获取选项式布局中的name，this是undefined
+    function changeName() {
+        name = 'Tom'
     }
-}
+    function changeAge() {
+        age += 1
+    }
+
+    function showPhone() {
+        alert(tel)
+    }
 </script>
 
 <style>
--- vite.config.ts
+++ vite.config.ts
@@ -3,12 +3,16 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
+// setup语法糖
+import vueSetupExtend from 'vite-plugin-vue-setup-extend'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
     vueDevTools(),
+    // vue setup语法糖
+    vueSetupExtend(),
   ],
   resolve: {
     alias: {
