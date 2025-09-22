81e9138122c861af143ae19cc8c8edb60663113d
BigDragonlin <51269337+BigDragonlin@users.noreply.github.com>
2025-09-22 14:43:37 +0800

    hash模式和history模式

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,11 +1,14 @@
 // 引入vue-router
-import { createRouter, createWebHistory} from 'vue-router';
+import { createRouter, createWebHistory, createWebHashHistory} from 'vue-router';
 import Home from '@/pages/Home.vue';
 import News from '@/pages/News.vue';
 import About from '@/pages/About.vue';
 
 // 创建路由实例
 const router = createRouter({
+    // 哈希模式:带#号,不利于seo,不用后端额外配置
+    // history: createWebHashHistory(),//哈希模式
+    // 历史模式:不带#号,利于seo,需要后端额外配置
     history: createWebHistory(),//历史模式
     routes: [
         {
