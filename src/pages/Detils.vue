1f527abce35bff2ca93d9ea2be6f69dee3713312
BigDragonlin <51269337+BigDragonlin@users.noreply.github.com>
2025-09-22 15:55:21 +0800

    路由query参数

--- src/pages/Detils.vue
+++ src/pages/Detils.vue
@@ -1,10 +1,12 @@
 <template>
-    <li>编号xxx</li>
-    <li>标题xxx</li>
-    <li>内容xxx</li>
+    <li>编号{{ route.query.id }}</li>
+    <li>标题{{ route.query.title }}</li>
+    <li>内容{{ route.query.content }}</li>
 </template>
 
 <script setup lang="ts" name="Detils">
+    import { useRoute } from 'vue-router';
+    const route = useRoute();
 </script>
 
 <style scoped>
--- src/pages/News.vue
+++ src/pages/News.vue
@@ -3,7 +3,7 @@
         <!-- 遍历news数据 -->
         <ul>
             <li v-for="item in news" :key="item.id">
-                <RouterLink to="/news/detail">{{ item.title }}</RouterLink>
+                <RouterLink :to="`/news/detail?id=${item.id}&title=${item.title}&content=${item.content}`">{{ item.title }}</RouterLink>
             </li>
         </ul>
     </div>
@@ -15,9 +15,9 @@
 <script setup lang="ts" name="News">
     import { reactive } from 'vue';
     const news = reactive([
-        { id: 1, title: '新闻1'},
-        { id: 2, title: '新闻2'},
-        { id: 3, title: '新闻3'},
+        { id: 1, title: '新闻1', content: '新闻1内容'},
+        { id: 2, title: '新闻2', content: '新闻2内容'},
+        { id: 3, title: '新闻3', content: '新闻3内容'},
     ]);
 </script>
 
