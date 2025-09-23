a2b5cf39482df434ed04ba1ec49b1c785d419a77
BigDragonlin <51269337+BigDragonlin@users.noreply.github.com>
2025-09-23 17:35:35 +0800

    store的组合是写法

--- src/store/LoveTalk.ts
+++ src/store/LoveTalk.ts
@@ -2,14 +2,30 @@ import { defineStore } from 'pinia'
 import axios from 'axios';
 import { nanoid } from 'nanoid';
 
-export const useLoveTalkStore = defineStore('loveTalk', {
-    actions: {
-        async getLoveTalk() {
-            let {data: {content: title}} = await axios.get('https://api.uomg.com/api/rand.qinghua?format=json');
-            this.talkList.unshift({id: nanoid(), info: title});
-        }
-    },
-    state: () => ({
-        talkList: JSON.parse(localStorage.getItem('loveTalkList') || '[]')
-    })
+// export const useLoveTalkStore = defineStore('loveTalk', {
+//     actions: {
+//         async getLoveTalk() {
+//             let {data: {content: title}} = await axios.get('https://api.uomg.com/api/rand.qinghua?format=json');
+//             this.talkList.unshift({id: nanoid(), info: title});
+//         }
+//     },
+//     state: () => ({
+//         talkList: JSON.parse(localStorage.getItem('loveTalkList') || '[]')
+//     })
+// })
+
+import { ref } from 'vue';
+export const useLoveTalkStore = defineStore('loveTalk', () => {
+    const talkList = ref(JSON.parse(localStorage.getItem('loveTalkList') || '[]'))
+
+    async function getLoveTalk() {
+        let {data: {content: title}} = await axios.get('https://api.uomg.com/api/rand.qinghua?format=json');
+        talkList.value.unshift({id: nanoid(), info: title});
+        localStorage.setItem('loveTalkList', JSON.stringify(talkList.value));
+    }
+
+    return {
+        talkList,
+        getLoveTalk
+    }
 })
