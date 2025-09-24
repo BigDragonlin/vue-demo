318d17e0a09c504856e02ef5f972ae112d35ba04
BigDragonlin <51269337+BigDragonlin@users.noreply.github.com>
2025-09-24 11:01:55 +0800

    初始化组件通信方式props

--- src/pages/01_props/Father.vue
+++ src/pages/01_props/Father.vue
@@ -1,22 +1,11 @@
 <template>
   <div class="father">
     <h3>父组件</h3>
-		<h4>汽车：{{ car }}</h4>
-		<h4 v-show="toy">子给的玩具：{{ toy }}</h4>
-		<Child :car="car" :sendToy="getToy"/>
   </div>
 </template>
 
 <script setup lang="ts" name="Father">
 	import Child from './Child.vue'
-	import {ref} from 'vue'
-	// 数据
-	let car = ref('奔驰')
-	let toy = ref('')
-	// 方法
-	function getToy(value:string){
-		toy.value = value
-	}
 </script>
 
 <style scoped>
