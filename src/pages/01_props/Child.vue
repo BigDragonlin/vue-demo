bc19cc819b99cbec7f2e03eca8ba40d6744cbf02
BigDragonlin <51269337+BigDragonlin@users.noreply.github.com>
2025-09-24 11:13:53 +0800

    组件通信方式props

--- src/pages/01_props/Child.vue
+++ src/pages/01_props/Child.vue
@@ -1,10 +1,16 @@
 <template>
   <div class="child">
     <h3>子组件</h3>
+	 <h4 v-if="car">父亲给的car：{{ car }}</h4>
+	 <h4>儿子有：{{ toy }}</h4>
+	 <button @click="sendToy(toy)">把玩具给父亲</button>
   </div>
 </template>
 
 <script setup lang="ts" name="Child">
+	import { ref } from 'vue'
+	defineProps(['car','sendToy'])
+	let toy = ref('奥特曼')
 </script>
 
 <style scoped>
--- src/pages/01_props/Father.vue
+++ src/pages/01_props/Father.vue
@@ -1,11 +1,25 @@
 <template>
   <div class="father">
     <h3>父组件</h3>
+	<h4>父亲有：{{ car }}</h4>
+	<h4 v-if="toy">儿子给的玩具：{{ toy }}</h4>
+	<button @click="sendCarToSon">把车给儿子</button>
+	<Child :car="sonCar" :sendToy="getToy"/>
   </div>
 </template>
 
 <script setup lang="ts" name="Father">
 	import Child from './Child.vue'
+	import { ref } from 'vue'
+	let car = ref('奔驰')
+	let sonCar = ref('')
+	let toy = ref('')
+	function sendCarToSon(){
+		sonCar.value = car.value
+	}
+	function getToy(value:string){
+		toy.value = value
+	}
 </script>
 
 <style scoped>
